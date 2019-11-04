286b4f3c5d19a210e3a733b1504f3ea2baa470fd
Basri Dogan <basri.dogan.71@gmail.com>
2019-11-04 14:29:22 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -8,9 +8,10 @@ def second_challenge
       :hero_friends=>[]
       }, 
     :capulet => {
-      #your key/value pairs here!
-      
-      }}
-
-  
+      patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+    }
 end
